c6eadbdf87343c72f06770f9c0042e95f55f0ebe
Natnael Eshetu <dragoneth.ion@gmail.com>
2020-11-09 01:40:28 +0300

    removed vector operations from template Vec2

--- include/axl.math/Vec2.hpp
+++ include/axl.math/Vec2.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "rules.hpp"
 
 namespace axl {
 namespace math {
@@ -44,31 +43,9 @@ class Vec2
 		bool equals(T x, T y, T epsilon = 0.0000001) const;
 		bool notEquals(T x, T y, T epsilon = 0.0000001) const;
 	public:
-		T magn() const;
-		Vec2<T> norm() const;
-		Vec2<T>& normalize();
-		T dot(const Vec2<T>& vec) const;
-		T cross(const Vec2<T>& vec) const;
-		T angle(Rules::Axis axis_rule = DefaultAxisRule) const;
-		T angle(const Vec2<T>& ref_vec, Rules::Axis axis_rule = DefaultAxisRule) const;
-		T fullAngle(Rules::Axis axis_rule = DefaultAxisRule) const;
-		T fullAngle(const Vec2<T>& ref_vec, Rules::Axis axis_rule = DefaultAxisRule) const;
-	public:
-		Vec2<T>& scale(T scale_all);
-		Vec2<T>& scale(T scale_x, T scale_y);
-		Vec2<T> scaled(T scale_all) const;
-		Vec2<T> scaled(T scale_x, T scale_y) const;
-		Vec2<T>& translate(T delta_x, T delta_y);
-		Vec2<T>& translate(const Vec2<T>& delta);
-		Vec2<T> translated(T delta_x, T delta_y) const;
-		Vec2<T> translated(const Vec2<T>& delta) const;
-		Vec2<T>& rotate(T theta_z, Rules::Axis axis_rule = DefaultAxisRule);
-		Vec2<T> rotated(T theta_z, Rules::Axis axis_rule = DefaultAxisRule) const;
-	public:
 		const static Vec2<T> Zero;
 		const static Vec2<T> Identity;
 		static Vec2<T> Default;
-		static Rules::Axis DefaultAxisRule;
 	public:
 		T x, y;
 };
--- src/lib.cpp
+++ src/lib.cpp
@@ -4,7 +4,7 @@ namespace axl {
 namespace math {
 namespace lib {
 
-const Version version = {.major = 0U, .minor = 4U, .patch = 0U};
+const Version version = {.major = 0U, .minor = 4U, .patch = 2U};
 
 } // namespace axl.math.lib
 } // namespace axl.math
